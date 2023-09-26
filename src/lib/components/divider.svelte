035517ad2148e4de7f1ef04afdab2d4a12eaef58
Fayez Kassem <101155960+Zoz55@users.noreply.github.com>
2023-09-27 01:37:25 +0300

    adding btn hover effect and media query

---
+++ src/lib/components/divider.svelte
@@ -0,0 +1,50 @@
+<script>
+</script>
+
+<div class="pdd">
+  <svg width="444" height="50" xmlns="http://www.w3.org/2000/svg"
+    ><g fill="none" fill-rule="evenodd"
+      ><path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" /><g
+        transform="translate(212)"
+        fill="#CEE3E9"
+        ><rect width="6" height="16" rx="3" /><rect
+          x="14"
+          width="6"
+          height="16"
+          rx="3"
+        /></g
+      ></g
+    ></svg
+  >
+</div>
+<div class="pdm">
+  <svg width="295" height="40" xmlns="http://www.w3.org/2000/svg"
+    ><g fill="none" fill-rule="evenodd"
+      ><path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" /><g
+        transform="translate(138)"
+        fill="#CEE3E9"
+        ><rect width="6" height="16" rx="3" /><rect
+          x="14"
+          width="6"
+          height="16"
+          rx="3"
+        /></g
+      ></g
+    ></svg
+  >
+</div>
+
+<style>
+  .pdm {
+    display: none;
+  }
+  @media (max-width: 500px) {
+    .pdm {
+      display: contents;
+    }
+
+    .pdd {
+      display: none;
+    }
+  }
+</style>
